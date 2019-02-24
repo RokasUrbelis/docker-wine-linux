09d7e246e91e1c5d418ea43f566f1b2f05443126
北慕城南 <rokas.yang@gmail.com>
2019-02-24 19:43:31 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ git clone https://github.com/wszqkzqk/deepin-wine-ubuntu.git
 ##########build docker image
 sudo docker build -t docker-wine-linux ./ && sed -i '2,7s/^/#&/g;9,11s/^/#&/g' $0
 sudo docker run -d -ti -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY  -e GDK_SCALE -e GDK_DPI_SCALE \
-	--name docker-wine-$RANDOM docker-wine-ubuntu /bin/bash|awk '{print substr($0,1,3)}'|tee docker.id &>/dev/null
+	--name docker-wine-$RANDOM docker-wine-linux /bin/bash|awk '{print substr($0,1,3)}'|tee docker.id &>/dev/null
 
 dockerid=$(cat docker.id)
 awk 'BEGIN{printf "Your container id is ";system("cat docker.id");printf "\nRun [source start.sh -i '" $dockerid "'";printf "TIM] to run TIM or another APP\n"}'

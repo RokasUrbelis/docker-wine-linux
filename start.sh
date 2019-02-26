6cfc6684158b7da7e1dde960f2c6fcebdaa4b81c
北慕城南 <rokas.yang@gmail.com>
2019-02-26 22:24:01 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,8 @@
 function RUN_APP() {
 	sudo docker exec -ti $1 /usr/bin/nohup /bin/bash &>/dev/null $2 &
  }
-case ${1} in
-	-i)
-		RUN_APP $2 $3
-		;;
-	 *)
+function USAGE() {
+
 		echo 'Usage:'
 		echo '		-i  [Docker container id or name] [APP Name]'
 		echo '		APP Nmae list:'
@@ -16,4 +13,32 @@ case ${1} in
 		echo ' 			      BaiduNetDisk'
 		echo '			      ThunderSpeed'
 		echo 'Example: source start.sh -i 0af TIM'
-esac
+}
+APP_LIST=(
+QQ
+TIM
+WeChat
+BaiduNetDisk
+ThunderSpeed
+Foxmail
+)
+if [ $# != 3 ];then
+	USAGE
+else 
+	case ${1} in
+		-i)
+			shift ##ID
+			if { cat demo.txt|& grep $1; } &>/dev/null;then
+				ID=$1
+				shift ##APP
+			        { for i in ${APP_LIST[@]};do echo $i;done |& grep -i "^${1}$"; } &>/dev/null　&&　RUN_APP ${ID} $2 \
+				|| echo "Sorry,'$1' not in list"
+			else
+				echo "Sorry,not found docker container id '$1'"
+			fi
+			;;
+	 	*)
+			USAGE
+			;;
+	esac
+fi

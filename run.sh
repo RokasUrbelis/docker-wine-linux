6e55449c718e0b3b6955f2a30698dd9934ec1738
Acoustroal北慕城南 <lonlyterminals@gmail.com>
2019-03-23 13:21:10 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ echo -e "[\033[32m\033[5m+\033[0m]Cloning speed may be slow, please waitting..."
 git clone https://github.com/wszqkzqk/deepin-wine-ubuntu.git
 
 ##########build docker image
-sudo docker build -t docker-wine-linux ./ && sed -i '2,7s/^/#&/g;9,13s/^/#&/g' $0
+sudo docker build -t docker-wine-linux ./ && sed -i '4,9s/^/#&/g' $0
 
 #########create docker container
 mkdir -p $(pwd)/APP_PATH && sudo docker run -d -ti -v $(pwd)/APP_PATH:/root -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY  -e GDK_SCALE -e GDK_DPI_SCALE \

df2942f5aab6e02f2a6e04aa276bccee7f7123a9
Acoustroal北慕城南 <lonlyterminals@gmail.com>
2019-04-01 23:55:50 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ echo -e "[\033[32m\033[5m+\033[0m]Cloning speed may be slow, please waitting..."
 git clone https://github.com/wszqkzqk/deepin-wine-ubuntu.git
 
 ##########build docker image
-if sudo docker build -t docker-wine-linux ./;then
+if sudo docker build -t docker-wine-linux ./; then
 	sed -i '4,14s/^/#&/g' $0
 else
 	printf "build docker image error,exit process\n"
@@ -16,7 +16,7 @@ fi
 function CREATE() {
 mkdir -p $(pwd)/APP_PATH
 if sudo docker run -d -ti -v $(pwd)/APP_PATH:/root -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY  -e GDK_SCALE -e GDK_DPI_SCALE \
- --name docker-wine-$RANDOM docker-wine-linux /bin/bash|awk '{print substr($0,1,3)}'|tee docker.id &>/dev/null
+ --name docker-wine-$RANDOM docker-wine-linux /bin/bash|awk '{print substr($0,1,3)}'|tee docker.id &>/dev/null; then
 	dockerid=$(cat docker.id)
 	return 0
 else
@@ -26,7 +26,7 @@ fi
 }
 CREATE
 code=$?
-if [ "$code" == "0" ];then
+if [ "$code" == "0" ]; then
 	awk 'BEGIN{printf "Your container id is ";system("cat docker.id && echo");system("echo -n [\033[32m\033[5m+\033[0m]");\
 	printf "Run [source start.sh -i '" $dockerid "'";printf "TIM] to run TIM or another APP\n"}'
 	echo

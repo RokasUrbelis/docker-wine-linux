a30940cba4d6c9551ccf96c1f773aa1cb3962f8e
Anthony·北慕城南 <lonlyterminals@gmail.com>
2019-10-13 18:04:13 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -e
 echo -e "[\033[32m\033[5m+\033[0m]Cloning speed may be slow, please waitting..."
 DIR="deepin-wine-ubuntu"
 [ -d $DIR ] && rm -rf $DIR 
-git clone https://github.com/wszqkzqk/${DIR}.git
+git clone https://github.com/RokasUrbelis/${DIR}.git
 ##########build docker image
 if sudo docker build -t docker-wine-linux ./; then
 	sed -i '4,14s/^/#&/g' $0
